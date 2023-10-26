0000b2a87e9e5fcbb4cbebd9cd1ec1536a395245
friedman <eliot.friedmann@etu.u_paris.fr>
2023-10-26 17:57:55 +0200

    equal et init ajouté

--- lib/engine.ml
+++ lib/engine.ml
@@ -1,4 +1,4 @@
-type player
+type player = X | O
 type xpos = X of int
 type ypos = Y of int
 type pos = xpos * ypos
@@ -9,17 +9,25 @@ module Pos = struct
   let y i = Y i
 end
 
-(* Pretty printers *)
 
-let equal_xpos a b = ignore (a, b); true
-let equal_ypos a b = ignore (a, b); true
-let equal_pos a b = ignore (a, b); true
+let equal_xpos (X i) (X j) =  i = j
+let equal_ypos (Y i) (Y j) = i = j
+let equal_pos (x1,y1) (x2,y2)= (equal_xpos x1 x2) && (equal_ypos y1 y2)
 
 exception Invalid_xpos
 exception Invalid_ypos
 exception Invalid_move
 
-let init () = []
+let init b = 
+  assert (List.length b = 8);
+  assert (List.for_all (fun line -> List.length line = 8) b); 
+  b
+
+let new_board = 
+  let l = List.init 3 (fun _ -> List.init 8 (fun _ -> None )) in
+  let centers = (List.init 3 (fun _ -> None)) @ [Some O;Some X] @ (List.init 3 (fun _ -> None)) in 
+  init (l @ [centers] @ ([List.rev centers]) @ l)
+
 
 let get b p =
   ignore (b, p);

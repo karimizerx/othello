5c5ec2052fc77d3b22252c67ff2f83df691baebf
MOREL victor <victor.morel@etu.u-paris.fr>
2023-10-27 09:33:18 +0200

    Delete engine.ml

--- test/engine.ml
+++
@@ -1,7 +0,0 @@
-let t =
-  Alcotest.test_case "test-template" `Quick (fun () ->
-      Alcotest.(check string) "same result" "Bonjour" ("Bon" ^ "jour"))
-
-let () =
-  let open Alcotest in
-  run "Template" [ ("test", [ t ]) ]

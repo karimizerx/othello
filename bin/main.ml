3423f958522b1e4c441d4828b862f020e83588c9
CRAGUE ilian <ilian.crague@etu.u-paris.fr>
2023-11-14 09:54:09 +0100

    Update main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,6 @@ open Omizer2mizer.Engine
 let () =
   Random.self_init ();
   Format.open_vbox 0;
-  game player_teletype player_invalid2 new_board;
+  game player_teletype player_random new_board;
   Format.close_box ();
   Format.printf "@."
